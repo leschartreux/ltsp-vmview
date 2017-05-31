b694262b29a38d7bf36be3bebc177364ecf110da
Raphaël RIGNIER <r.rignier@leschartreux.net>
2017-05-31 12:11:46 +0200

    Update for LTSP PNP

--- setupvmview-ltsp-pnp.sh
+++ setupvmview-ltsp-pnp.sh
@@ -7,28 +7,32 @@
 
 DIR=$1
 ARCH=i386
-CHROOT="/"
-VMVIEW_PATH="/usr/lib/vmware/view/"
+CHROOT=""
+VMVIEW_PATH="/usr/lib/vmware/"
 TFTPDIR="/var/lib/tftpboot"
 SETUP_ARGS_FILE="vhc-setup-args.conf"
 
 CLIENT_BUNDLE=`ls VMware-Horizon-Client*.bundle 2> /dev/null`
 
-#Check if C
+echo "---------------------------------------------------------------------------"
+#Check if Client Bundle is downloaded
 if [ -z $CLIENT_BUNDLE ]; then
 	echo "VMware-Horizon-Client bundle file not Found";
 	echo "please visit http://www.vmware.com/go/viewclients and download last release for Linux";
 	exit 1;
 fi
 
-if [ ! -f  $VMVIEW_PATH ]; then
+#Version specific settings and librairies
+. $SETUP_ARGS_FILE
+
+
+if [ ! -d  $VMVIEW_PATH ]; then
 	echo "Found $CLIENT_BUNDLE try to install it"
 	cp $CLIENT_BUNDLE $CHROOT/root/
 	chmod a+x $CHROOT/root/$CLIENT_BUNDLE
-	if [ ! -f $SETUP_ARTGS_FILE ]; then
+	if [ ! -f $SETUP_ARGS_FILE ]; then
 		echo "$SETUP_ARGS_FILE Not found. Can't install"
 	else
-		. $SETUP_ARTGS_FILE
 		/root/$CLIENT_BUNDLE $VMSETUP_ARGS
 	fi
 	      
@@ -37,37 +41,49 @@ else
 	echo "Continue"
 fi
 
+echo "---------------------------------------------------------------------------"
 #REquired packages for vmware-view
 for i in ltsp-server ltsp-client ldm libxss1 openssl openssh-server x11vnc hsetroot openbox
 do
 	echo install package $i
 	apt-get install $i --yes
 done
+echo "---------------------------------------------------------------------------"
 
-echo "link libraries"
-#if [ ! -f "$CHROOT/$VMVIEW_PATH/libssl.so.1" ]; then
-#	ln -s /usr/lib/vmware/view/usb/libssl.so.1.0.1 /lib/$ARCH-linux-gnu/libssl.so.1.0.1
-#fi
-#if [ ! -f "$CHROOT/$VMVIEW_PATH/libcrypto.so.1.0.1" ]; then
-#	ln -s /usr/lib/vmware/view/usb/libcrypto.so.1.0.1 /lib/$ARCH-linux-gnu/libcrypto.so.1.0.1
-#fi
-#if [ ! -f "$CHROOT/$VMVIEW_PATH/libudev.so.0" ]; then
-#	ln -s /lib/i386-linux-gnu/libudev.so.1 /lib/$ARCH-linux-gnu/libudev.so.0
-#fi
+echo install dependencies librairies
+apt-get install $VMW_DEPENDENCIES --yes
+echo "--------------------------------------------------------------------------"
+
+#generate lib symlinks from VM_OLDLIBS Var
+for l in $VMW_LIBLINKS
+do
+	echo "link librairies"
+	LD=`echo $l | cut -d"=" -f1`
+	LS=`echo $l | cut -d"=" -f2`        
+	
+	if [ ! -f "$CHROOT/$LD" ]; then
+
+		echo "ln -s $LS $CHROOT$LD"
+		ln -s $LS $CHROOT$LD
+	fi
+done
+echo "---------------------------------------------------------------------------"
 
 
-echo "copy localuser profile"
+
 if [ ! -d /home/localuser ]; then
 	echo creating new user localuser
 	useradd -m localuser
 	echo "localuser:user" | chpasswd
 fi
 echo "copy localuser profile"
-cp -rv localuser $CHROOT/
+cp -av localuser $CHROOT/home/
+echo "---------------------------------------------------------------------------"
 
 echo "copy ltsp scripts"
 cp -rv xinitrc.d $CHROOT/usr/share/ltsp/
 cp -rv screen.d $CHROOT/usr/share/ltsp/
+echo "---------------------------------------------------------------------------"
 
 
 #echo "Change kernel generator config"
@@ -80,6 +96,7 @@ cp -rv screen.d $CHROOT/usr/share/ltsp/
 #fi
 
 #Needed to include root home dir into squashfs image (unbuntu)
+echo "---------------------------------------------------------------------------"
 if [ -f /etc/ltsp/ltsp-update-image.excludes ]; then
 	echo "Change update-image exlcudes"
 	if [ ! -f /etc/ltsp/update-image.excludes.bak ]; then
@@ -88,31 +105,32 @@ if [ -f /etc/ltsp/ltsp-update-image.excludes ]; then
 	fi
 fi
 
-if [ ! -f $TFTPDIR/ltsp/$DIR/lts.conf ]; then
+if [ ! -f $TFTPDIR/ltsp/$ARCH/lts.conf ]; then
 	echo "copy lts.conf file in TFTP root"
-	cp lts.conf $TFTPDIR/ltsp/$DIR/lts.conf
+	cp lts.conf $TFTPDIR/ltsp/$ARCH/lts.conf
 fi
+echo "---------------------------------------------------------------------------"
 
 
 echo "**************************"
 echo "SETUP FINISHED !"
 echo "**************************"
 echo "things to be done : "
-echo "	1) edit view.autoconnectBroker in $CHROOT/root/.vmware/view-preferences"
+echo "	1) edit view.autoconnectBroker in /home/localuser/.vmware/view-preferences"
 echo ""
 echo "	2) optionally add a VNC password to access screen remotely :"
-echo "ltsp-chroot --arch $DIR x11vnc -storepasswd /root/.config/openbox/.vncpass"
+echo "x11vnc -storepasswd /home/localuser/.config/openbox/.vncpass"
 echo ""
-echo "	3) optionally create root password for console or SSH access:"
-echo "ltsp-chroot --arch $DIR passwd"
+#echo "	3) optionally create root password for console or SSH access:"
+#echo "ltsp-chroot --arch $DIR passwd"
+#echo ""
+echo "	3) rebuild your squashfs image with"
+echo "ltsp-update-image --cleanup /"
 echo ""
-echo "	4) rebuild your squashfs image with"
-echo "ltsp-update-image $DIR"
-echo ""
-echo "	5) edit Broker IP in $TFTPDIR/ltsp/$DIR/lts.conf"
+echo "	4) edit Broker IP in $TFTPDIR/ltsp/$ARCH/lts.conf"
 echo 
-echo "	6) Configure some thin client in your dhcpd.conf (isc-dhcp-server) by adding those parameters : "
+echo "	5) Configure some thin client in your dhcpd.conf (isc-dhcp-server) by adding those parameters : "
 echo "		next-server ltsp_server_ip;"
-echo "		filename \"ltsp/$DIR/pxelinux.0\";"
+echo "		filename \"ltsp/$ARCH/pxelinux.0\";"
 echo
 echo Enjoy !
